213b29ea8f5bb5e6570992761893070c3ce78a2a
sbuis <samuel.buis@inra.fr>
2020-03-17 09:40:49 +0100

    Create test-stics-wrapper.R

---
+++ tests/testthat/test-stics-wrapper.R
@@ -0,0 +1,33 @@
+# Test parameter forcing works
+
+param_names=c("dlaimax","durvieF")
+param_lb=c(0.0005,50)
+param_ub=c(0.0025,400)
+var_name="lai_n"
+situation_name="bo96iN+"
+path_to_JavaStics="D:/Home/sbuis/Documents/GitHub/CroptimizR/vignettes/"   # A CHANGER POUR DOWNLOAD DEPUIS GOOGLEDRIVE
+javastics_path=file.path(path_to_JavaStics,"JavaSTICS-1.41-stics-9.0")
+stics_path=file.path(javastics_path,"bin/stics_modulo.exe")
+data_dir= file.path(SticsRFiles::download_data(),"study_case_1","V9.0")
+stics_inputs_path=file.path(data_dir,"TxtFiles")
+dir.create(stics_inputs_path)
+SticsRFiles::gen_usms_xml2txt(javastics_path = javastics_path, workspace_path = file.path(data_dir,"XmlFiles"),
+                 target_path = stics_inputs_path, display = TRUE)
+model_options=SticsOnR::stics_wrapper_options(stics_path,stics_inputs_path,
+                                    parallel=FALSE)
+wrapper=SticsOnR::stics_wrapper
+param_values_min <- array( param_lb,
+                           dim=c(1,length(param_lb),1),
+                           dimnames=list(NULL,param_names,situation_name))
+param_values_max <- array( param_ub,
+                           dim=c(1,length(param_ub),1),
+                           dimnames=list(NULL,param_names,situation_name))
+sim_max       <- wrapper(param_values = param_values_min, model_options = model_options)
+sim_min       <- wrapper(param_values = param_values_max, model_options = model_options)
+
+test_that("Parameter forcing works", {
+  expect_gt(sum( abs(sim_max$sim_list[[1]][[situation_name]][,var_name]-
+                       sim_min$sim_list[[1]][[situation_name]][,var_name]),
+                 na.rm=TRUE),
+            0)
+})
