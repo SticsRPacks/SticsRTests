3cee68b4e89a0ad437deab1d17856049aa801a24
Samuel Buis <samuel.buis@inrae.fr>
2020-07-07 16:52:19 +0200

    temporary modif for debugging purpose

--- tests/testthat/test-estim_param.R
+++ tests/testthat/test-estim_param.R
@@ -14,6 +14,7 @@ SticsRFiles::gen_usms_xml2txt(javastics_path = javastics_path, workspace_path = 
 sit_name="bo96iN+"  # can be a vector of situation names if you want to consider several, e.g. c("bo96iN+","bou00t1")
 var_name="lai_n"    # can be a vector of variable names if you want to consider several, e.g. c("lai_n","masec_n")
 obs_list= SticsRFiles::get_obs(javastics_workspace_path, usm_name = sit_name)
+class(obs_list) <- append("list", "stics_observation")
 
 print(obs_list)
 flush.console()
