f28f461a5e52ae8dc887ce57fc456cec376ea4f5
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2024-12-20 17:12:45 +0100

    Fix test tutorial (#6)
    * fixing cores numbers if tests are done in Github CI.
    
    * fix: cores number in the context of Github CI
    
    * update of the test-tutorial: using now the tutorial Rmd from the SticsRPacks library, some of the functions' return have been masked, tests are based on 2 use case with or without a local javastics installation
    
    * missing import package
    
    * removed use of import

--- tests/testthat/test-tutorial.R
+++ tests/testthat/test-tutorial.R
@@ -1,36 +1,105 @@
+options(warn=-1)
 
-#Download and transform Tutorial .RMD to .R
-tmpdir <- normalizePath(tempdir(), winslash = "/", mustWork = FALSE)
-tutorial_rmd <- file.path(tmpdir, "SticsRpacks.Rmd")
-download.file("https://raw.githubusercontent.com/SticsRPacks/SticsRPacks/main/inst/tutorials/SticsRpacks/SticsRpacks.Rmd",
-              tutorial_rmd)
-xfun::gsub_file(file = tutorial_rmd,
-                "eval=FALSE","eval=TRUE",
-                fixed = TRUE)
+# import::from(xfun, xfun::gsub_file)
+# import::from(knitr, purl)
+library(xfun)
+library(knitr)
 
-xfun::gsub_file(file = tutorial_rmd,
-                "optim_options = list(nb_rep = 3, out_dir = workspace_path)",
-                "optim_options = list(nb_rep = 3, out_dir = workspace_path, maxeval = 3)",
-                fixed = TRUE)
+# Getting the tutorial path from the SticsRPacks library
+tutorial_rmd <- normalizePath(
+  system.file("tutorials/SticsRpacks/SticsRpacks.Rmd", package = "SticsRPacks"),
+  mustWork = TRUE
+)
+
+# Making a copy of the tutorial
+tutorial_test_rmd <- "SticsRpacks_test.Rmd"
+file.copy(from = tutorial_rmd,
+          to = tutorial_test_rmd)
+
+# gsub some parts of the tutorial for activating chunks evaluation
+# and masking some functions return objects
+xfun::gsub_file(file = tutorial_test_rmd,
+          "eval=FALSE","eval=TRUE",
+          fixed = TRUE)
+
+xfun::gsub_file(file = tutorial_test_rmd,
+          "optim_options = list(nb_rep = 3, out_dir = workspace_path)",
+          "optim_options = list(nb_rep = 3, out_dir = workspace_path, maxeval = 3)",
+          fixed = TRUE)
 
 if (Sys.getenv("CI") != "") {
-  xfun::gsub_file(file = tutorial_rmd,
-                  "parallel = TRUE","parallel = TRUE, cores = 2",
-                  fixed = TRUE)
+  xfun::gsub_file(file = tutorial_test_rmd,
+            "parallel = TRUE","parallel = TRUE, cores = 2",
+            fixed = TRUE)
 }
 
-tutorial_r <-file.path(tmpdir, "tutorial.R")
-knitr::purl(input = tutorial_rmd,
-            output = tutorial_r,
-            documentation = 2)
+# removing outputs from get_sim
+xfun::gsub_file(file = tutorial_test_rmd,
+          "get_sim(workspace = workspace_path)",
+          "x <- get_sim(workspace = workspace_path)",
+          fixed = TRUE)
+
+xfun::gsub_file(file = tutorial_test_rmd,
+          'get_sim(workspace = workspace_path, usm = c("banana", "Turmeric"))',
+          'x <- get_sim(workspace = workspace_path, usm = c("banana", "Turmeric"))',
+          fixed = TRUE)
+
+xfun::gsub_file(file = tutorial_test_rmd,
+          'get_sim(workspace = workspace_path, usm = c("wheat", "maize"), var = "lai_n")',
+          'x <- get_sim(workspace = workspace_path, usm = c("wheat", "maize"), var = "lai_n")',
+          fixed = TRUE)
+
+xfun::gsub_file(file = tutorial_test_rmd,
+          "print(sim)",
+          "",
+          fixed = TRUE)
+
+xfun::gsub_file(file = tutorial_test_rmd,
+          "print(p)",
+          "",
+          fixed = TRUE)
 
 
-# Test Tutorial
-test_tuto <- function() {
-  source(tutorial_r)
+xfun::gsub_file(file = tutorial_test_rmd,
+          "print(res1)",
+          "",
+          fixed = TRUE)
+
+xfun::gsub_file(file = tutorial_test_rmd,
+          "print(res2)",
+          "",
+          fixed = TRUE)
+
+# Creating a R script from the tutorial Rmd file
+tutorial_test_r <- "tutorial_test.R"
+
+knitr::purl(input = tutorial_test_rmd,
+     output = tutorial_test_r,
+     documentation = 2)
+
+
+# Running the Tutorial script for
+# Default use case with downloading JavaStics from the forge
+test_tuto_download_javastics <- function() {
+  javastics_in_path <- Sys.setenv(javastics_path="")
+  source(tutorial_test_r)
+  return (TRUE)
+}
+# Local JavaStics installation use case
+test_tuto_local_javastics <- function() {
+  version <- get_stics_versions_compat()$latest_version
+  javastics_path <- system.file(paste0("stics/", version),
+                                package = "SticsRTests")
+  Sys.setenv(javastics_path = javastics_path)
+  source(tutorial_test_r)
   return (TRUE)
 }
 
 test_that("Test Tutorial", {
-  expect_equal(test_tuto(), TRUE)
+  expect_equal(test_tuto_download_javastics(), TRUE)
+  expect_equal(test_tuto_local_javastics(), TRUE)
 })
+
+unlink(tutorial_test_rmd)
+
+unlink(tutorial_test_r)
